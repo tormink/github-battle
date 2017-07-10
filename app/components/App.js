bd483673c6800ea376d12c5ff90b5c50560dfd78
Lucas Tormin <lucasntormin@gmail.com>
2017-07-10 00:30:51 -0300

    Implemented dynamic rendering functionality onto Battle component and started the Results component

--- app/components/App.js
+++ app/components/App.js
@@ -8,6 +8,7 @@ var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
 var Popular = require('./Popular');
+var Results = require('./Results');
 
 class App extends React.Component {
     render() {
@@ -19,6 +20,7 @@ class App extends React.Component {
                         <Route exact path="/" component={Home}/>
                         <Route path="/popular" component = {Popular}/>
                         <Route exact path="/battle" component = {Battle} />
+                        <Route path="/battle/results" component = {Results} />
                         <Route render={function(){
                             return (
                                 <p>
